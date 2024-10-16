76a7a32ac79e5254b8c53dff8355316d98111d7a
Hariom-vishwa <hawkgamerhv@gmail.com>
2024-10-16 13:28:36 +0530

    changes

--- src/Fashion.jsx
+++ src/Fashion.jsx
@@ -7,19 +7,19 @@ export function Fashion() {
       category: "Men's Fashion",
       photo:
         "https://i0.wp.com/armenyl.com/wp-content/uploads/2021/09/Mens-StreetStyle-at-New-York-Fashion-Week-photo-by-Armenyl.jpg",
-      id: "menfashion",
+      id: "/fashion/menfashion",
     },
     {
       category: "Women's Fashion",
       photo:
         "https://i0.wp.com/mylook.com.de/wp-content/uploads/2024/07/90s-streetwear-outfit-for-women-2OzzdviIReuo5lZ74v5OxQ-ZCm4ASxpSc6MDKWdDReSNQ.jpeg",
-      id: "womenfashion",
+      id: "/fashion/womenfashion",
     },
     {
       category: "Kid's Fashion",
       photo:
         "https://i.pinimg.com/736x/de/2e/69/de2e6960806d35b03f5a303e5d13b475.jpg",
-      id: "kidsfashion",
+      id: "/fashion/kidsfashion",
     },
   ];
 
