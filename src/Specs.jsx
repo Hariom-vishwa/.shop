206ca00ba3ff09434edce56bf027e706a0ed46a1
Hariom-vishwa <hawkgamerhv@gmail.com>
2024-11-07 12:08:48 +0530

    New Changes

--- src/Specs.jsx
+++ src/Specs.jsx
@@ -16,50 +16,21 @@ export default function Specs() {
         </div>
         <div className="rightPart">
           <h1>Item Name: {prod.name}</h1>
-          <p>Price: <strong>₹{prod.price}/-</strong></p>
-          <p>Color: {prod.color}</p>
-          <p>Size: {prod.size}</p>
-          
-          {Object.entries(prod.details).map(([key, value]) => (
-            <p key={key}>
-              {key}: {value}
+          <div className="rightSpecs">
+            <p>
+              <strong>Price:</strong> <strong>₹{prod.price}/-</strong>
             </p>
-          ))}
+            <p><strong>Color:</strong> {prod.color}</p>
+            <p><strong>Size:</strong> {prod.size}</p>
 
+            {Object.entries(prod.details).map(([key, value]) => (
+              <p key={key}>
+                <strong>{key}:</strong> {value}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </>
   );
 }
-
-// // Specs.js
-// import React from "react";
-// import { useParams } from "react-router-dom";
-// import { womensWear } from "./Women";
-
-// export default function Specs() {
-//   const { productName } = useParams();
-//   const prod = womensWear.find((pro) => pro.name === productName);
-
-//   if (!prod) return <h2>Product not found</h2>;
-
-//   return (
-//     <>
-//       <div>
-//         <img src={prod.photo} alt="" />
-//         <h1>{prod.name}</h1>
-//         <p>Price: ₹{prod.price}/-</p>
-//         <p>Color: {prod.color}</p>
-//         <p>Size: {prod.size}</p>
-//         <h3>Details:</h3>
-//         {/* <ul>
-//           {Object.entries(prod.details).map(([key, value]) => (
-//             <li key={key}>
-//               <strong>{key}:</strong> {value}
-//             </li>
-//           ))}
-//         </ul> */}
-//       </div>
-//     </>
-//   );
-// }
